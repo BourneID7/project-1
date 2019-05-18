c57920efa48bb058d7c91664ff1ae2cfc0ea9dea
Jason Bourne <m.bourne@wi.rr.com>
2019-05-18 15:35:29 -0500

    integrated Hanna's giphy query with js & updated css for gif display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,65 @@
 $(document).ready(function() {
 
-  // hide results screen at page load
-$("#resultsDisplay").hide();
+    // hide results screen at page load
+  $("#resultsDisplay").hide();
 
-// on submit hide form screen & show results screen
-$("#submit").on("click", function(event) {
-   event.preventDefault(); 
+  // on submit hide form screen & show results screen
+  $("#submit").on("click", function(event) {
+    event.preventDefault(); 
 
-   var userChoices = $(this).data();
-   var price = $("#priceRange").val();
-   var food = $("#foodType").val();
-   var distance = $("#distance").val();
-   var city = $("#city").val().trim().toLowerCase();
-        
-    // var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + city + "&price_range" + price + "&apikey=fc365d62a0c922660dbdd5fbb407fa71"
+    // query zomato api to get restaurant results
+    var userChoices = $(this).data();
+    var price = $("#priceRange").val();
+    var food = $("#foodType").val();
+    var distance = $("#distance").val();
+    var city = $("#city").val().trim().toLowerCase();
+          
+      // var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + city + "&price_range" + price + "&apikey=fc365d62a0c922660dbdd5fbb407fa71"
 
-    var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + city + "&count=10&radius=" + distance + "&cuisines="+ food + "&apikey=fc365d62a0c922660dbdd5fbb407fa71"
+      var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + city + "&count=10&radius=" + distance + "&cuisines="+ food + "&apikey=fc365d62a0c922660dbdd5fbb407fa71"
 
-
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-  
-    }).then(function(response) {      
-
-      console.log(response); 
-
-
+      $.ajax({
+        url: queryURL,
+        method: "GET"
     
-    });
+      }).then(function(response) {      
 
-$("#formDisplay").hide();
-$("#resultsDisplay").show();
+        console.log(response); 
+      
+      });
 
-})
+      // query giphy api to get images of chosen type of food
+
+      function displayGif() {
+        
+        var chosenfood = $("#foodType").val() + " food"
+        console.log (chosenfood)
+        var queryURL2 = "https://api.giphy.com/v1/gifs/search?api_key=Kc3B8vMettxiRAYXD1ePoUIDMSYc4Tf3&q="+chosenfood+"&limit=10&offset=0&rating=G&lang=en"
+        
+        $.ajax({
+          url: queryURL2,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response);
+
+          var results = response.data;
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var gifurl = results[i].images.fixed_width.url;
+            var gifpic= $('<img class="gif">');
+            gifpic.attr("src", gifurl);
+            gifpic.attr("alt", "food");
+            $("#gifs").append(gifDiv);
+            gifDiv.append(gifpic);
+        }
+      })
+      }
+
+    $("#formDisplay").hide();
+    $("#resultsDisplay").show();
+    displayGif();
+
+  })
 
 
 })
