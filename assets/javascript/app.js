072d01a17a027b4baf3f53a296c36ab887855d08
Habib <auni93085@gmail.com>
2019-05-18 12:32:56 -0500

    changes to api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,36 @@
 $(document).ready(function() {
 
-    $("button").on("click", function() {
+  // hide results screen at page load
+$("#resultsDisplay").hide();
 
-      var foodType = $(this).data();
+// on submit hide form screen & show results screen
+$("#submit").on("click", function(event) {
+   event.preventDefault(); 
+
+   var userChoices = $(this).data();
         
-        var queryURL = " https://api.yelp.com/v3/businesses/{id}" +
-          foodType + "&api_key=EO8evVJvdxgy_jWvbaXxTBz6u0MUF_AXfohi1MGgMHIymv6UTm53rrKWDqTy-JsELgptEAAn6t7Gl77n_QrFktV-zgmte1-3ENdVKxcEOhS7PZzYlaZG_cgY3wDeXHYx&limit=10";
-    
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {      
-    
-          console.log(response);
+    var queryURL = "https://developers.zomato.com/api/v2.1/categories?apikey=fc365d62a0c922660dbdd5fbb407fa71"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+  
+    }).then(function(response) {      
+
+      console.log(response);
+
+      
+     
+
+});
+
+$("#formDisplay").hide();
+$("#resultsDisplay").show();
+
+})
+
+
+})
 
 
          
@@ -27,4 +45,3 @@ $(document).ready(function() {
 
 
 
-})
