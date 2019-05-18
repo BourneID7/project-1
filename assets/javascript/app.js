e08d56e0604bba41d8278b91e1675fd1a1a19f73
Habib <auni93085@gmail.com>
2019-05-18 14:10:24 -0500

    made changes to js. got api key to return values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,8 +8,15 @@ $("#submit").on("click", function(event) {
    event.preventDefault(); 
 
    var userChoices = $(this).data();
+   var price = $("#priceRange").val();
+   var food = $("#foodType").val();
+   var distance = $("#distance").val();
+   var city = $("#city").val().trim().toLowerCase();
         
-    var queryURL = "https://developers.zomato.com/api/v2.1/categories?apikey=fc365d62a0c922660dbdd5fbb407fa71"
+    // var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + city + "&price_range" + price + "&apikey=fc365d62a0c922660dbdd5fbb407fa71"
+
+    var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + city + "&count=10&radius=" + distance + "&cuisines="+ food + "&apikey=fc365d62a0c922660dbdd5fbb407fa71"
+
 
     $.ajax({
       url: queryURL,
@@ -17,9 +24,11 @@ $("#submit").on("click", function(event) {
   
     }).then(function(response) {      
 
-      console.log(response);     
+      console.log(response); 
 
-});
+
+    
+    });
 
 $("#formDisplay").hide();
 $("#resultsDisplay").show();
